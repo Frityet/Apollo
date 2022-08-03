d7b00240bfaefd59d64a345350fd4e9fbf4f4976
Fira <frodggyxd@gmail.com>
2022-08-03 10:06:46 -0400

    removed stuff from package.lua

--- src/package.lua
+++ src/package.lua
@@ -1,14 +1,14 @@
 local package = {
-	name         = "lpl",
-	version      = { major = 0, minor = 1, patch = 0 },
+	name         = "",
+	version      = { major = 0, minor = 0, patch = 0 },
 	authors      = {
-		"Frityet",
-		"0xFira"
+		"",
+		""
 	},
-	license      = "GPL-3.0",
-	description  = "A next generation package manager for lua",
-	homepage     = "https://github.com/Frityet/Lua-Package-Manager",
-	repository   = "https://github.com/Frityet/Lua-Package-Manager",
+	license      = "",
+	description  = "",
+	homepage     = "",
+	repository   = "",
 	dependencies = {
 		optional = {}
 	},
