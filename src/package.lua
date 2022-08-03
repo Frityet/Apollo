bb3297d26b35777f222589cf8613bc9f0315ddb6
Fira <frodggyxd@gmail.com>
2022-08-03 10:26:10 -0400

    last time ong

--- src/package.lua
+++ src/package.lua
@@ -1,15 +1,14 @@
 ---@class Package
 local export = {
-	package_version = 1,
-	name            = "",
-	version         = {},
-	authors         = {},
-	license         = "",
-	description     = "",
-	lua_versions    = {},
-	dependencies    = {},
-	on_build        = function(package) end,
-	on_install      = function(package) end,
+	name 			= "",
+	version 		= {},
+	authors 		= {},
+	license 		= "",
+	description 	= "",
+	lua_versions	= {},
+	dependencies 	= {},
+	on_build = function(package) end,
+	on_install = function (package) end,
 }
 
 return export
