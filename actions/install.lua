dcedaeb46bf4f568245a4f06e4cd75f55a5eca93
Amrit Bhogal <ambhogal01@gmail.com>
2022-08-07 14:02:42 -0700

    Restarting async request

--- actions/install.lua
+++ actions/install.lua
@@ -12,17 +12,5 @@ return function (args)
 		queue:add("GET", "https://gist.githubusercontent.com/truemedian/9f52aedd31ab5952da980b0e02b3a3d8/raw/d3b42159ddc7d0d9d2523eb62720b18b66570832/dload.lua")
 	}
 
-	local ret = queue:wait();
-	switch (type(ret)) {
-		["table"] = function ()
-			print(ret)
-		end,
-		["thread"] = function ()
-			print(coroutine.resume(ret --[[@as thread]]))
-		end,
-		default = function ()
-			error("Unexpected type to return (" .. type(ret) .. ")", 1)
-			return nil
-		end
-	}
+	queue:wait()
 end
