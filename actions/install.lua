f6b20c4230ee3637c49f88d1bfa4ef0c7d34e01e
Amrit Bhogal <ambhogal01@gmail.com>
2022-08-08 14:45:41 -0700

    aaaaaaaa

--- actions/install.lua
+++ actions/install.lua
@@ -7,8 +7,6 @@ local RequestQueue = require("../net/async-request")
 return function (args)
 	local queue = RequestQueue.create()
 	local reqs = {
-		queue:add("GET", "https://gist.githubusercontent.com/truemedian/9f52aedd31ab5952da980b0e02b3a3d8/raw/d3b42159ddc7d0d9d2523eb62720b18b66570832/dload.lua"),
-		queue:add("GET", "https://gist.githubusercontent.com/truemedian/9f52aedd31ab5952da980b0e02b3a3d8/raw/d3b42159ddc7d0d9d2523eb62720b18b66570832/dload.lua"),
 		queue:add("GET", "https://gist.githubusercontent.com/truemedian/9f52aedd31ab5952da980b0e02b3a3d8/raw/d3b42159ddc7d0d9d2523eb62720b18b66570832/dload.lua")
 	}
 
--- net/async-request.lua
+++ net/async-request.lua
@@ -1,11 +1,105 @@
 local http = require("coro-http")
 local utilities = require("utils")
 
----@class AsyncRequest
-local AsyncRequest = {}
+---@class RequestQueue
+local export = {
+    ---@class RequestQueue.Request
+    ---@field body string
+    ---@field head string
 
-function AsyncRequest:create()
-    
+    ---@type RequestQueue.Request[] | thread[]
+    requests = {
+        body = "",
+        head = ""
+    },
+    finished = 0,
+    waiting_all = {},
+    waiting_individual = {}
+}
+
+export.__index = export
+
+---@return RequestQueue
+function export.create()
+    local self = setmetatable({}, export)
+
+    self.requests = {}
+    self.finished = 0
+
+    self.waiting_all = {}
+    self.waiting_individual = {}
+
+    return self
 end
 
-return AsyncRequest
+---@param id integer
+---@return nil
+function export:_notify(id)
+    if self.waiting_individual[id] then
+        for _, thread in ipairs(self.waiting_individual[id]) do
+            utilities.assertResume(thread, self.requests[id])
+        end
+    end
+
+    if self.finished == #self.requests then
+        for _, thread in ipairs(self.waiting_all) do
+            utilities.assertResume(thread)
+        end
+    end
+end
+
+---@param id integer    
+---@param method string
+---@param url string
+---@param headers string[]?
+---@param payload string?
+function export:_request(id, method, url, headers, payload)
+    local res, body = http.request(method, url, headers, payload)
+
+    self.requests[id] = { head = res, body = body }
+    self.finished = self.finished + 1
+
+    self:_notify(id)
+end
+
+---Adds a request to the queue.
+---@param method "GET" | "POST" | "PUT" | "DELETE"
+---@param url string
+---@param headers string[]?
+---@param payload string?
+---@return integer
+function export:add(method, url, headers, payload)
+    local id = #self.requests + 1
+    self.requests[id] = coroutine.create(export._request)
+
+    assert(type(self.requests[id]) == "thread")
+    coroutine.resume(self.requests[id]--[[@as thread]], self, id, method, url, headers, payload)
+
+    return id
+end
+
+---@param id integer?
+---@return RequestQueue.Request | thread?
+function export:wait(id)
+    if id == nil then
+        if self.finished == #self.requests then
+            return nil
+        else
+            table.insert(self.waiting_all, (coroutine.running()))
+
+            return coroutine.yield()
+        end
+    else
+        if type(self.requests[id]) == 'thread' then
+            self.waiting_individual[id] = self.waiting_individual[id] or {}
+
+            table.insert(self.waiting_individual[id], (coroutine.running()))
+
+            return coroutine.yield()
+        else
+            return self.requests[id]
+        end
+    end
+end
+
+return export
